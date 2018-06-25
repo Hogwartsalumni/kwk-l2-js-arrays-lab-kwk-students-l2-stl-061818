8aadbcf7dad0dbbec04e72cb8cb8044286a5d8f8
Scarlett Patton <scarwars05@gmail.com>
2018-06-25 13:42:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift(name);
+}
+
+function appendDriver(name) {
+  drivers.concat(name);
+  drivers();
+}
+
+function prependDriver(name) {
+  
+}
+
+function removeLastDriver(name) {
+  newDrivers = drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver(name) {
+  drivers.slice(1);
+  drivers;
+}
