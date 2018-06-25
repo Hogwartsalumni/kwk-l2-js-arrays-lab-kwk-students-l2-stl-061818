24aef7939768b40e746618d2b617ea3071aec721
Scarlett Patton <scarwars05@gmail.com>
2018-06-25 14:05:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift(name);
+}
+
+function appendDriver(name) {
+  const newDriver = [...drivers, 'Broom'];
+  return newDriver;
+}
+
+function prependDriver(name) {
+  const newStart = ['Arnold', ...drivers];
+  return newStart;
+}
+
+function removeLastDriver(name) {
+  const newDrivers = drivers.slice(0, drivers.length - 1);
+  return newDrivers;
+}
+
+function removeFirstDriver(name) {
+  const removeFirst = drivers.slice(-2);
+  return removeFirst;
+}
